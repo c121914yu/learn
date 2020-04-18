aa7dc2aa43929b03c445d338c832c2591cb30297
yjl <545436317@qq.com>
2020-04-18 09:53:31 +0800

    高德地图

--- README.md
+++ README.md
@@ -1,36 +1,49 @@
-#LearnDemo
+# 高德地图
 
-1. accordion_slide - 滑动手风琴
-2. animatedIcon - 字体图标动画
-3. axiosAxios - 网络请求库
-4. blur_effect - 背景模糊切换
-5. canvas_base - 画布canvas基本语法
-6. css_gridcss - 网格布局
-7. css_unitecss - 单位
-8. color_board - 色彩板
-9. dropdown_menu - 下拉菜单
-10. dragImg - 图片拖拽
-11. fliter_search - 过滤搜索
-12. food_web_demo - 移动端外卖静态网页
-13. game_airplane_battle - 飞机大战
-14. game_snake - 贪吃蛇
-15. git_learngit - 基本语法
-16. img_slider - 图片轮播
-17. loading_animation - 三种加载动画
-18. nav_animation - 导航栏动画
-19. randmPsw - 随机密码生成器
-20. regular_express - 正则表达式
-21. rotatePPT - 旋转幻灯片
-22. ripple_cicle - 波纹小球
-23. search_states - 州级行政区查询
-24. split_landing_page - 主次界面切换效果（放大缩小）
-25. starRating - 星级评价
-26. textTOvoice - 文字转语言API
-27. text_background_animation - 文字背景动画
-28. topNav - 响应式顶部导航栏（下拉导航）
-29. thumbnail - 缩略图
-30. vision_difference - 滚动视觉差（背景固定）
-31. weight_converter - 重量转换器
-32. search_states - 行政区查询
-33. compass - 鼠标指南针
-34. gaodeMap - 高德地图
+```js
+// 创建地图
+var map = new AMap.Map(domID, {
+	zoom: 14, //初始化地图层级,越大越细
+	resizeEnable: true, //是否监控地图容器尺寸变化
+	center: [120.147723, 30.244324] //初始化地图中心点
+})
+
+// 创建一个标记点
+var marker = new AMap.Marker({
+	position: pos
+})
+
+// 创建一条曲线
+let polyline = new AMap.Polyline({
+	path: path,  
+	borderWeight: 2, // 线条宽度，默认为 1
+	strokeColor: 'red', // 线条颜色
+	lineJoin: 'round' // 折线拐点连接处样式
+})
+```
+
+> map - API
+> getCity(callback) - 获取当前城市信息
+> setCity(cityname,code) - 跳转指定城市
+> getCenter() - 获取地图中心点坐标
+> add() - 添加覆盖物
+> remove() - 移除覆盖物
+> on(type,callback) - 添加监听事件
+
+
+```js
+//西湖经纬度：
+[120.147723, 30.244324]
+
+// 西湖周围维度
+[
+	[120.135975, 30.250522],
+	[120.138024, 30.243867],
+	[120.139494, 30.239251],
+	[120.144397, 30.229120],
+	[120.154466, 30.231864],
+	[120.154332, 30.236114],
+	[120.154836, 30.238727],
+	[120.158747, 30.24768]
+]
+```
---
+++ js/init.js
@@ -0,0 +1,110 @@
+// 创建地图
+var map = new AMap.Map('container', {
+	zoom: 14, //初始化地图层级,越大越细
+	resizeEnable: true, //是否监控地图容器尺寸变化
+	center: [120.147723, 30.244324] //初始化地图中心点,若不写则默认是当前位置
+})
+
+//获取当前城市信息
+map.getCity(res => {
+	console.log(res)
+})
+ 
+// 跳转指定城市
+// map.setCity("杭州市")
+
+// 获取当前地图中心点
+const circle = map.getCenter()
+
+// 添加标记
+var marker
+function addMark(pos){
+ //if(marker)
+	//map.remove(marker) // 移除标记
+ marker = new AMap.Marker({
+		position: pos
+ })
+ map.add(marker)
+}
+
+// 根据点绘制曲线
+function drawLine(path){
+	let polyline = new AMap.Polyline({
+		path: path,  
+		borderWeight: 2, // 线条宽度，默认为 1
+		strokeColor: 'red', // 线条颜色
+		lineJoin: 'round' // 折线拐点连接处样式
+	})
+	map.add(polyline)
+	// 展示 / 隐藏
+	//polyline.show()
+	//polyline.hide()
+}
+// drawLine()
+
+// 路线规划
+function pathPlan(){
+	AMap.plugin('AMap.Walking', () => {
+		var driving = new AMap.Walking({
+			map,
+			autoFitView: true
+		})
+		
+		let points = [
+			[120.135975, 30.250522],
+			[120.138024, 30.243867],
+			[120.139494, 30.239251],
+			[120.144397, 30.229120],
+			[120.154466, 30.231864],
+			[120.154332, 30.236114],
+			[120.154836, 30.238727],
+			[120.158747, 30.24768]
+		]
+		
+		driving.search(points[7], points[0],(status, result) => {
+			const routes = result.routes[0]
+			console.log("总路程：" + routes.distance + "米")
+			console.log("预计时间: " + (routes.time/60).toFixed(1) + "分钟")
+		})
+	})
+}
+pathPlan()
+
+// 获取当前位置坐标
+function getSelfPosition(){
+	AMap.plugin('AMap.Geolocation', function() {
+	  var geolocation = new AMap.Geolocation({
+	    // 是否使用高精度定位，默认：true
+	    enableHighAccuracy: true,
+	    // 设置定位超时时间，默认：无穷大
+	    timeout: 10000,
+	    //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+	    zoomToAccuracy: true,     
+			// 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
+			buttonOffset: new AMap.Pixel(10, 20),
+	    //  定位按钮的排放位置,  RB表示右下
+	    buttonPosition: 'RB'
+	  })
+	
+	  geolocation.getCurrentPosition()
+	  AMap.event.addListener(geolocation, 'complete', onComplete)
+	  AMap.event.addListener(geolocation, 'error', onError)
+	
+	  function onComplete (data) {
+			addMark(data.position)
+			console.log(data)
+	  }
+	  function onError (err) {
+	    // 定位出错
+			console.log(err)
+	  }
+	})
+}
+// getSelfPosition()
+
+// 地图点击事件
+// map.on('click',e => {
+// 	const pos = [e.lnglat.getLng(),e.lnglat.getLat()]
+// 	addMark(pos)
+// 	console.log(pos)
+// })
