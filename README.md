e6a39de3df537e1f44696fd13b1770b885bb2805
yjl <545436317@qq.com>
2020-07-13 17:13:51 +0800

    vue3Demo

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
 # vue3demo
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+> vue add vue-next 升级 vue3 项目
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<p>姓名: {{ name }}</p>
+		<p>
+			<button @click="changeAge(-1)">-</button>
+			年龄: {{ age }}
+			<button @click="changeAge(1)">+</button>
+		</p>
+		<p>
+			<button @click="changeYear(-1)">-</button>
+			出生年份: {{ year }}
+			<button @click="changeYear(1)">+</button>
+		</p>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { computed, reactive, toRefs, watch } from "vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	props: {
+		title: {
+			type: String,
+			default: "555",
+		},
+	},
+	// Composition API入口，在beforeCreate之前调用，无this
+	setup(props, context) {
+		// const name = ref("余金隆") //非字符串，而是响应式对象,值用.value获取
+		// const age = ref(18)
+		// const year = computed({
+		// 	get: () => {
+		// 		return 2020 - age.value
+		// 	},
+		// 	set: (val) => {
+		// 		age.value = 2020 - val
+		// 	},
+		// })
+		// 使用data统一构造对象
+		const data = reactive({
+			name: "余金隆",
+			age: 18,
+			year: computed({
+				get: () => {
+					return 2020 - data.age
+				},
+				set: (val) => {
+					data.age = 2020 - val
+				},
+			}),
+		})
+		function changeAge(n) {
+			data.age += n
+			console.log(props.title)
+		}
+		watch(
+			() => data.age,
+			(newTitle, oldTitle) => {
+				console.log(newTitle, oldTitle)
+				console.log(context)
+				// context.emit("changeAge")
+			}
+		)
+		function changeYear(val) {
+			data.year += val
+		}
+		return { ...toRefs(data), changeAge, changeYear }
+	},
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
